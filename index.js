628fc4a2bb0dd32a3a60d60253e00a1bacf1435a
Donat Pllana <donat.pllana@outlook.com>
2020-02-15 20:41:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function whisper(string){
 function logShout(string){
   console.log(string.toUpperCase());
 }
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string)
+{
+  if(string.toUpperCase())
+  {
+    return "YES INDEED!"
+  }
+}
